4aa722dfec53a12ddc45e6eed05e87ad92b1657f
naga vadlapudi <nagain2017@outlook.com>
2023-10-02 13:36:26 -0400

    Updated lateFee

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,12 @@
-import { Component,EventEmitter,Output,SimpleChanges } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 import { IDevice } from '../models/IDevice';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { environment } from 'src/environments/environment';
@@ -9,71 +17,81 @@ import { SummaryService } from '../services/summary.service';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
 })
-export class MainComponent{
-
-  constructor(private router: Router,private summaryService: SummaryService){}
+export class MainComponent {
+  constructor(private router: Router, private summaryService: SummaryService) {}
 
   devices: IDevice[] = environment.devices;
   summary: ISummary[] = [];
 
-  finalBill: number=0;
-  totalMobileDataCharges: number=0;
-  showDevices: boolean=false;
-  displaySaveButton: boolean=true;
-  displayResetButton: boolean=false;
+  finalBill: number = 0;
+  totalMobileDataCharges: number = 0;
+  showDevices: boolean = false;
+  displaySaveButton: boolean = true;
+  displayResetButton: boolean = false;
+  totalDataCharge: number = 0;
+  lateFee: number =0;
 
   basicInfoForm = new FormGroup({
-    totalDataCharges: new FormControl('',[Validators.required]),
+    totalDataCharges: new FormControl('', [Validators.required]),
+    lateFee: new FormControl(''),
     totalNumberOfDevices: new FormControl(this.devices.length),
-    dataChargePerDevice: new FormControl(0)
-  })
+    dataChargePerDevice: new FormControl(0),
+  });
 
   dataChargePerDevice: number = 0;
 
-  ngOnChanges(changes: SimpleChanges) {
-    console.log(changes['totalDataCharges'].currentValue);
+  onBasicFormSubmit() {
+    this.displaySaveButton = false;
+    this.displayResetButton = true;
+    this.showDevices = true;
+    this.finalBill = 0;
   }
 
-  onBasicFormSubmit(){
-    this.displaySaveButton=false;
-    this.displayResetButton=true;
-    this.showDevices=true;
-    this.finalBill=0;
+  setTotalDataCharge(e: any) {
+    this.totalDataCharge = e.target.value;
+    this.calculateSharedBill(this.totalDataCharge);
   }
 
-  calculateCharges(e:any){
-    let totalDataCharges=e.target.value;
-    this.dataChargePerDevice=totalDataCharges/this.devices.length;
-    this.dataChargePerDevice=Number(this.dataChargePerDevice.toFixed(2));
-    this.basicInfoForm.controls['dataChargePerDevice'].setValue(this.dataChargePerDevice);
-    this.devices.forEach(device=>{
-        device.dataCharge=this.dataChargePerDevice;
-    })
-    this.totalMobileDataCharges=totalDataCharges;
+  setLateFee(e: any) {
+    this.lateFee = e.target.value;
+    this.calculateSharedBill(Number(this.totalDataCharge) + Number(this.lateFee));
   }
 
-  calculateFinalBill(e:any){
-    this.finalBill=this.finalBill+e;
-    this.finalBill=Number(this.finalBill.toFixed(2));
+  calculateSharedBill(totalDataCharges: number) {
+    this.dataChargePerDevice = totalDataCharges / this.devices.length;
+    this.dataChargePerDevice = Number(this.dataChargePerDevice.toFixed(2));
+    this.basicInfoForm.controls['dataChargePerDevice'].setValue(
+      this.dataChargePerDevice
+    );
+    this.devices.forEach((device) => {
+      device.dataCharge = this.dataChargePerDevice;
+    });
+    this.totalMobileDataCharges = totalDataCharges;
   }
-  displayFormData(form:any){
+
+  calculateFinalBill(e: any) {
+    this.finalBill = this.finalBill + e;
+    this.finalBill = Number(this.finalBill.toFixed(2));
+  }
+
+  displayFormData(form: any) {
     this.summary.push(form.value);
   }
 
-  checkSummary(){
+  checkSummary() {
     this.summaryService.setSummary(this.summary);
-    this.router.navigate(["/summary"]);
+    this.router.navigate(['/summary']);
   }
 
-  onResetForm(){
+  onResetForm() {
     this.basicInfoForm.reset();
-    this.showDevices= this.displayResetButton =false;
-    this.displaySaveButton=true;
-    this.finalBill=0;
-    this.basicInfoForm.controls['totalNumberOfDevices'].setValue(this.devices.length);
+    this.showDevices = this.displayResetButton = false;
+    this.displaySaveButton = true;
+    this.finalBill = 0;
+    this.basicInfoForm.controls['totalNumberOfDevices'].setValue(
+      this.devices.length
+    );
   }
-  
-
 }
